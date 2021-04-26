29edc822f0cdff6c5b7620499da8e21a44fdf119
graciellehigino <graciellehigino@hotmail.com>
2021-04-26 07:09:11 -0400

    :poop: no good beta-div attempt

---
+++ code/04-beta_div.jl
@@ -0,0 +1,21 @@
+# Carnivores
+## Original metaweb
+M[carnivores];
+
+## Subnetwork of linked carnivores
+linked_c = reduce(union, skipmissing(subnetworks_c))
+
+# Herbivores
+## Original metaweb
+M[herbivores];
+
+## Networks herbivores
+linked_h = reduce(union, skipmissing(subnetworks_h))
+
+# Beta-div
+## Composition
+#βs_c = βs(linked_c, M[carnivores])
+#βs_h = βs(linked_h, M[herbivores])
+
+#KGL01.([EcologicalNetworks.βs(i, j) for i in linked_c, j in M[carnivores]])
+
