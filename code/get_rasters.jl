cd6bd01d8058367a3a3b57077e16d1843f6c2b22
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-24 11:51:51 -0400

    ✨ code to get missing freshwater species raster files

--- code/get_rasters.jl
+++ code/get_rasters.jl
@@ -7,6 +7,7 @@ using DataFrames
 # I store my IUCN data in the same location, so adapt this to where your files are
 const IUCNPATH = expanduser(joinpath("~", ".data", "iucn"))
 const IUCNDB = "MAMMALS_TERRESTRIAL_ONLY"
+const IUCNFR = "MAMMALS_FRESHWATER"
 
 # Make a folder to store the rasters if needed
 ispath("rasters") || mkpath("rasters")
@@ -45,6 +46,28 @@ end
 
 # Get names of species with valid files
 mammals = speciespool[findall(valid_names)]
+
+# Get species from the MAMMALS_FRESHWATER data set
+freshwater = ["Hippopotamus amphibius", "Leptailurus serval"]
+
+Threads.@threads for i in 1:length(freshwater)
+    sp = freshwater[i]
+    @info "Extracting $(sp) on thread $(Threads.threadid())"
+    fname = joinpath("rasters", replace(sp, " " => "_")*".tif")
+    if !isfile(fname)
+        query = `gdal_rasterize -l "$(IUCNFR)" -a presence $(IUCNPATH)/$(IUCNFR)/$(IUCNFR).shp $(fname) -where "binomial LIKE '$(sp)'" -ts 2200, 1100`
+        run(query)
+        mp = convert(Float64, geotiff(SimpleSDMResponse, fname)); bounding_box...))
+        replace!(mp, zero(eltype(mp)) => nothing)
+        geotiff(fname, broadcast(v -> isnothing(v) ? v : one(eltype(mp)), mp))
+        mp = nothing
+        GC.gc()
+    end
+end
+
+# Add to species list
+append!(mammals, freshwater)
+sort!(mammals)
 
 # Export names
 CSV.write(joinpath("data", "mammals.csv"), DataFrame(mammals = mammals), header = false)
