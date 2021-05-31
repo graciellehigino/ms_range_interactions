18964a67b0216da07200f7de62e61ea29746eea1
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-31 14:49:54 -0400

    ⚡️ speed up & simplify GBIF script

--- code/06-GBIF_data.jl
+++ code/06-GBIF_data.jl
@@ -1,49 +1,50 @@
+# Load required scripts and packages
+include("01-load_rasters.jl") # range maps of Serengeti mammals 
+include("shapefile.jl") # mapping functions
+
 using GBIF
 
 # Getting taxa codes
-sp_codes = taxon.([i for i in mammals], rank = :SPECIES)
+sp_codes = taxon.(mammals, rank = :SPECIES)
 
 # Bounding box
 lat, lon = (bounding_box.bottom, bounding_box.top), (bounding_box.left, bounding_box.right)
 
 # Getting observations
 ## Start query
-occ = occurrences.(sp_codes,
-            "hasCoordinate" => "true",
-            "decimalLatitude" => lat,
-            "decimalLongitude" => lon,
-            "continent" => "AFRICA")
-## loop to get all occurrences
-while length.(occ) < size.(occ)
-    occurrences!.(occ)
-end
-
-# Making Data Frames
-occ_vector = collect.(view.(occ))
-occ_df = DataFrame.(occ_vector)
-
-# Getting points by species 
-occ_coordinates = DataFrame(fill(0, nrow(occ_df[1]), 3),
-[:species, :longitude, :latitude])
-
-occ_coordinates.longitude = occ_df[1].longitude
-occ_coordinates.latitude = occ_df[1].latitude
-occ_coordinates[!, :species] .= mammals[1]
-
-
-for i in 1:length(occ_df)
-    long = occ_df[i].longitude
-    lat = occ_df[i].latitude
-    species = mammals[i]
-    x = hcat(fill.(species, length(long)), long, lat)
-    x = DataFrame(x, :auto)
-    rename!(x, :x1 => :species, :x2 => :latitude, :x3 => :longitude)
-    if ~isdefined(occ_coordinates, 1) | (i == 1)
-        global occ_coordinates = x
-    else
-        append!(occ_coordinates, x)
+occ = occurrences.(
+    sp_codes,
+    "hasCoordinate" => "true",
+    "decimalLatitude" => lat,
+    "decimalLongitude" => lon,
+    "continent" => "AFRICA"
+)
+## Loop to get all occurrences
+Threads.@threads for o in occ
+    while length(o) < size(o)
+        occurrences!(o)
     end
+    @info "$(o.occurrences[1].taxon.name) occurrences returned ($(length(o))/$(size(o)))"
 end
+# Export to JLD2 for faster reload
+#=
+using JLD2
+@save joinpath("data", "clean", "gbif-occurrences.jld2") occ
+@load joinpath("data", "clean", "gbif-occurrences.jld2") occ
+=#
 
-# Only species with IUCN ranges
-occ_gbif_iucn = filter(x -> x.species ∈ names(names_df), occ_coordinates)
+# Make Data Frames
+occ_df = DataFrame.(occ)
+occ_df = reduce(vcat, occ_df)
+select!(occ_df, :species, :longitude, :latitude)
+
+# Make sure the species names match
+isequal(unique(occ_df.species), mammals) # not the same
+setdiff(unique(occ_df.species), mammals) # Taurotragus oryx is the difference
+
+id_mismatch = filter(x -> !(x.species in mammals), occ_df)
+unique(id_mismatch.species) # Yep it's only Taurotragus oryx
+# The GBIF website agrees it's a synonym of Tragelaphus oryx, citing IUCN as source, so we're good
+
+replace!(occ_df.species, "Taurotragus oryx" => "Tragelaphus oryx")
+isequal(unique(occ_df.species), mammals) # true
