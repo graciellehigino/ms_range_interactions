ffd40889310b40105469a18025a68d177d79ddd4
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2022-02-12 12:30:05 -0500

    🐛 fix range loss

--- code/08-GBIF_doublecheck.jl
+++ code/08-GBIF_doublecheck.jl
@@ -18,12 +18,13 @@ serval_updated = ranges_updated[findfirst(==("Leptailurus_serval"), mammals)]
 serval_gbif = gbif_ranges[findfirst(==("Leptailurus_serval"), mammals)]
 
 # Let's see what its distribution looks like
-plot(serval, c=:BuPu)
-plot(serval_updated, c=:BuPu)
-plot(serval_gbif, c=:BuPu) # not many points, maybe add some buffer?
+plot(serval, c=:BuPu, title="Original range")
+plot(serval_updated, c=:BuPu, title="Updated range")
+plot(serval_gbif, c=:BuPu, title="GBIF range") # not many points, maybe add some buffer?
 
 # Get the range loss
+_loss_sites = setdiff(keys(serval), keys(serval_updated))
 serval_loss = similar(serval)
-serval_loss[keys(serval_updated)] .= -1.0 # need to update SimpleSDMLayers?
-serval_loss[keys(serval_loss)]
-plot(serval_loss, c=:BuPu)
+serval_loss[_loss_sites] = fill(1.0, length(_loss_sites))
+serval_loss = replace(serval_loss, 0.0 => nothing)
+plot(serval_loss, c=:BuPu, title="Range loss")
