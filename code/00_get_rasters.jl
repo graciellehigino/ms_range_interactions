b076a89faf6ac0f95f6e154c8dc61d6a3fdccc04
graciellehigino <graciellehigino@gmail.com>
2022-09-08 13:50:51 -0700

    :art: :hammer: change resolution + change filtering to sp list

--- code/00_get_rasters.jl
+++ code/00_get_rasters.jl
@@ -11,8 +11,16 @@ ispath("rasters") || mkpath("rasters")
 bounding_box = (left=-20.0, right=55.0, bottom=-35.0, top=40.0)
 
 # Get the list of hosts
-speciespool = readlines(joinpath("data", "species.csv"))
-filter!(!endswith(" spp."), speciespool) # Species with spp. at the end are plants, so we can remove them
+# speciespool = readlines(joinpath("data", "species.csv"))
+# filter!(!endswith(" spp."), speciespool) # Species with spp. at the end are plants, so we can remove them
+
+# I like this code below better, even though it's longer! It does the same thing
+# and gives the same results, but it only deals with explicit information on the
+# DF, and we won't need to explain how do we know that species names with spp.
+# are plants, and why we decided to exclude some plants and not others.
+speciespool = DataFrame(CSV.File(joinpath("data", "species_code.csv")))
+filter!(x -> x.type != "plant", speciespool)
+speciespool = speciespool[:,:species]
 
 # Rename species following IUCN taxonomy
 replace!(speciespool, "Damaliscus korrigum" => "Damaliscus lunatus", "Taurotragus oryx" => "Tragelaphus oryx")
@@ -25,7 +33,7 @@ Threads.@threads for i in 1:length(speciespool)
     @info "Extracting $(sp) on thread $(Threads.threadid())"
     fname = joinpath("rasters", replace(sp, " " => "_")*".tif")
     try
-        query = `gdal_rasterize -l "$(IUCNDB)" -a presence $(IUCNPATH)/$(IUCNDB)/$(IUCNDB).shp $(fname) -where "binomial LIKE '$(sp)'" -tr 0.1666666666666666574 0.1666666666666666574 -te -180.0 -90.0 180.0 90.0`
+        query = `gdal_rasterize -l "$(IUCNDB)" -a presence $(IUCNPATH)/$(IUCNDB)/$(IUCNDB).shp $(fname) -where "binomial LIKE '$(sp)'" -tr 0.5 0.5 -te -180.0 -90.0 180.0 90.0`
         run(query)
         mp = convert(Float64, geotiff(SimpleSDMResponse, fname; bounding_box...))
         replace!(mp, zero(eltype(mp)) => nothing)
