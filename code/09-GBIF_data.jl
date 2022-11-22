6abb2931533c365054980915c4331076bd95756c
Francis Banville <francis.banville@umontreal.ca>
2022-11-22 17:19:42 -0500

    ✨ fix code and make fig on recent GBIF obs

--- code/09-GBIF_data.jl
+++ code/09-GBIF_data.jl
@@ -31,10 +31,10 @@ end
 
 ## Create DataFrame
 
-# Make Data Frames
+# Make DataFrame
 occ_df = DataFrame.(occ)
 occ_df = reduce(vcat, occ_df)
-select!(occ_df, :species, :longitude, :latitude)
+select!(occ_df, :species, :longitude, :latitude, :date)
 occ_df.species = replace.(occ_df.species, " " => "_")
 
 # Remove observations with coordinates around (0.0, 0.0) which are outside mainland
@@ -54,6 +54,7 @@ isequal(unique(occ_df.species), mammals) # true
 # Save as CSV
 CSV.write(joinpath("data", "clean", "gbif_occurrences.csv"), occ_df)
 
+
 ## Investigate difference with original query with Africa as continent
 # Load datasets
 old = CSV.read(joinpath("data", "clean", "gbif_occurrences_old.csv"), DataFrame) # get from previous commit
@@ -68,6 +69,7 @@ diff = @chain begin
     sort(:diff, rev=true)
 end
 diff
+
 
 ## Create layers
 
@@ -92,59 +94,94 @@ geotiff(joinpath("data", "clean", "gbif_occurrences.tif"), gbif_occ_layers)
 geotiff(joinpath("data", "clean", "gbif_ranges.tif"), gbif_ranges)
 
 
-# Explore year distribution
+## Explore year distribution
 
-#Get years
-using Dates
-
-occ_with_dates = dropmissing(occ_df, :date)
-occ_with_dates.year = year.(occ_with_dates.date)
-
-#Calculate mode per species
-Pkg.add("StatsBase")
+# Calculate median date for each species
 using StatsBase
+
 group_species = groupby(occ_with_dates, :species)
-mode_species = combine(group_species, :year => StatsBase.mode)
-Pkg.rm("StatsBase")
+median_species = combine(group_species, :year => StatsBase.median)
 
 
-# Plot the distribution of the occurrences' year of record per species
+# Plot the distribution of the occurrences' year of record for each species
 p = plot(layout = 32, size = (1200, 800), dpi= 600)
 
 for i in 1:32
     plot!(group_species[i].year, subplot = i, title = replace(levels(occ_df.species)[i], "_" => " "), titlefontsize = 7,
-    seriestype=:histogram, legends = :none, fillcolor = "black", bins = :scott, xlims = extrema(occ_with_dates.year))
-    vline!([transpose(mode_species.year_mode)], linecolor = :red)
-    
+    seriestype = :histogram, legends = :none, fillcolor = "black", bins = :scott, xlims = extrema(occ_with_dates.year))
+    vline!([transpose(median_species.year_median)], linecolor = :red)
 end
 p
 
 savefig(joinpath("figures", "GBIF_years_total.png"))
 
-# Distribution of the occurrences' year > 1900 per species
+# Distribution of the occurrences' year > 1900 for each species
+occ_with_dates_1900 = subset(occ_with_dates, :year => ByRow(>(1900)))
+
+group_species_1900 = groupby(occ_with_dates_1900, :species)
+median_species_1900 = combine(group_species_1900, :year => StatsBase.median)
 
 p = plot(layout = 32, size = (1200, 800), dpi= 600)
-subset(group_species[1], :year => ByRow(>(1900)))
+
 for i in 1:32
-    group_species_subset = subset(group_species[i], :year => ByRow(>(1900)))
-    plot!(group_species_subset.year, subplot = i, title = replace(levels(occ_df.species)[i], "_" => " "), titlefontsize = 7,
-    seriestype=:histogram, legends = :none, fillcolor = "black", nbins = maximum(occ_with_dates.year) - 1900, xlims = (1900, maximum(occ_with_dates.year)), xr = 45, tickfontsize = 6)
-    vline!([transpose(mode_species.year_mode)], linecolor = :red)
+    plot!(group_species_1900[i].year, subplot = i, title = replace(levels(occ_df.species)[i], "_" => " "), titlefontsize = 7,
+    seriestype = :histogram, legends = :none, fillcolor = "black", nbins = maximum(occ_with_dates.year) - 1900, xlims = (1900, maximum(occ_with_dates.year)), xr = 45, tickfontsize = 6)
+    vline!([transpose(median_species_1900.year_median)], linecolor = :red)
 end
 p
 
 savefig(joinpath("figures", "GBIF_years_1900.png"))
 
-# Distribution of the occurrences' year > 1970 per species
+# Distribution of the occurrences' year > 1970 for each species
+
+occ_with_dates_1970 = subset(occ_with_dates, :year => ByRow(>(1970)))
+
+group_species_1970 = groupby(occ_with_dates_1970, :species)
+median_species_1970 = combine(group_species_1970, :year => StatsBase.median)
 
 p = plot(layout = 32, size = (1200, 800), dpi= 600)
-subset(group_species[1], :year => ByRow(>(1900)))
+
 for i in 1:32
-    group_species_subset = subset(group_species[i], :year => ByRow(>(1970)))
-    plot!(group_species_subset.year, subplot = i, title = replace(levels(occ_df.species)[i], "_" => " "), titlefontsize = 7,
-    seriestype=:histogram, legends = :none, fillcolor = "black", nbins = 50, xlims = (maximum(occ_with_dates.year)-50, maximum(occ_with_dates.year)), xr = 45, tickfontsize = 6)
-    vline!([transpose(mode_species.year_mode)], linecolor = :red)
+    plot!(group_species_1970[i].year, subplot = i, title = replace(levels(occ_df.species)[i], "_" => " "), titlefontsize = 7,
+    seriestype = :histogram, legends = :none, fillcolor = "black", nbins = 50, xlims = (maximum(occ_with_dates.year)-50, maximum(occ_with_dates.year)), xr = 45, tickfontsize = 6)
+    vline!([transpose(median_species.year_median)], linecolor = :red)
 end
 p
 
-savefig(joinpath("figures", "GBIF_years_1972.png"))
+savefig(joinpath("figures", "GBIF_years_1970.png"))
+
+
+# Proportion of recent occurrences for each species 
+year_recent = 2000
+
+occ_with_dates_recent = subset(occ_with_dates, :year => ByRow(>(year_recent)))
+group_species_recent = groupby(occ_with_dates_recent, :species)
+
+prop_recent = [nrow(group_species_recent[i]) / nrow(group_species[i]) for i in 1:32] 
+
+plot(prop_recent, 
+    seriestype = :histogram, legends = :none, fillcolor = "darkred", nbins = 50, xlims = (0, 1), ylims=(0, 6), xr = 45, tickfontsize = 6,
+    xlabel = "Proportion of GBIF occurrences after $(year_recent)",
+    ylabel = "Frequency", 
+    guidefontsize=8)
+
+savefig(joinpath("figures", "GBIF_years_prop_2000.png"))
+
+
+
+# Proportion and number of recent occurrences for each species
+nb_recent = [nrow(group_species_recent[i]) for i in 1:32] 
+
+scatter(prop_recent, nb_recent,
+    legends = :none, xr = 45, tickfontsize = 6,
+    color = "darkred", markersize = 6, 
+    xlims = (0, 1), ylims = (0, 9500),
+    xlabel = "Proportion of GBIF occurrences after $(year_recent)",
+    ylabel = "Number of GBIF occurrences after $(year_recent)",
+    guidefontsize=8)
+
+savefig(joinpath("figures", "GBIF_years_prop_nb_2000.png"))
+
+
+
+
